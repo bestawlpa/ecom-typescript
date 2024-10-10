fd4bcbb5bde0bfd21342d7ef2b2600fe2d8582ef
apiwat <bunderscorest@gmail.com>
2024-10-10 17:23:31 +0700

    update-Header

--- front-TypeScript/src/component/Header.tsx
+++ front-TypeScript/src/component/Header.tsx
@@ -6,13 +6,13 @@ const Header = () => {
   return (
     <div style={{width: '100%', height: '100px', background: '#1A4D2E', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
         <div style={{width:'1200px', height:'80px', display:'flex', justifyContent:'space-between', alignItems:'center'}}>
-            <div>
+            <div style={{width:'280px'}}>
                 <Link to={'/'} style={{textDecoration:'none'}}>
                     <h1 style={{color:'#FAF7F0'}}>NightMarket</h1>
                 </Link>
             </div>
-            <div style={{height:'30px', width:'240px', display:'flex', justifyContent:'space-between',alignItems:'center', background:'#FFFFFF', borderRadius:'8px', overflow:'hidden'}}>
-                <input type="text" placeholder='search' style={{width:'192px', outline:'none', border:'none', borderRadius:'8px', height:'26px', padding:'0 6px'}}/>
+            <div style={{height:'30px', width:'300px', display:'flex', justifyContent:'space-between',alignItems:'center', background:'#FFFFFF', borderRadius:'8px', overflow:'hidden'}}>
+                <input type="text" placeholder='search' style={{width:'250px', outline:'none', border:'none', borderRadius:'8px', height:'26px', padding:'0 6px'}}/>
                 <button style={{background:'#FFFFFF',  border: 'none', borderLeft: '2px solid #000000', height:'24px', display:'flex', alignItems:'center', justifyContent:'center'}}>
                     <img src="./header/search-svgrepo-com (2).svg" alt="img" style={{width:'22px', height:'22px'}}/>
                 </button>
