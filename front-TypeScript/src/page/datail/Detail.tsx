6072a54cadc735fbfbc56051d0fbea46bd1712ef
apiwat <bunderscorest@gmail.com>
2024-10-15 16:23:10 +0700

    update-Home,Detail

--- front-TypeScript/src/page/datail/Detail.tsx
+++ front-TypeScript/src/page/datail/Detail.tsx
@@ -1,15 +1,70 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import Header from '../../component/Header';
 import Footer from "../../component/Footer";
 import { useParams } from "react-router-dom";
 
+
+interface Product {
+  id: number; 
+  product_name: string;
+  price: number;
+  images: string[]
+}
 const Detail = () => {
-    const { id } = useParams();
+  const { id } = useParams();
+  const [product, setProduct] = useState<Product | null>(null);
+  console.log(product);
+  
+
+  const getProduct = async () => {
+    try {
+      const response = await fetch(`http://localhost:3030/api/products/${id}`)
+      if(!response.ok){
+        throw new Error('product not found')
+      }
+      const data = await response.json();
+      console.log('data',data);
+      
+      setProduct(data)
+    } catch (error) {
+      console.error('Internal Server Error')
+    }
+  }
+
+  useEffect(() => {
+    getProduct()
+  },[]);
+
   return (
-    <div>
+    <div className="container">
       <Header />
-      <div style={{display:'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '16px', width:'1000px', height:'100vh', margin:'20px'}}>
+      <div style={{display:'flex', justifyContent:'center', background:'red',alignItems:'start', width:'1000px', height:'100vh', margin:'40px 0'}}>
         <h1>Detail of Product ID: {id}</h1>
+        <div>
+           {product ? (
+          <div style={{ maxWidth: '800px', textAlign: 'left' }}>
+            <h2>{product.product_name}</h2>
+            <p><strong>Category:</strong> {product.category_name}</p>
+            <p><strong>Description:</strong> {product.description}</p>
+            <p><strong>Price:</strong> ${product.price}</p>
+            <p><strong>Inventory:</strong> {product.inventory_quantity} left</p>
+            <p><strong>Rating:</strong> {product.rating} stars</p>
+            <div>
+              <h3>Images</h3>
+              {product.images.map((image, index) => (
+                <img
+                  key={index}
+                  src={image}
+                  alt={product.product_name}
+                  style={{ width: '150px', marginRight: '10px' }}
+                />
+              ))}
+            </div>
+          </div>
+        ) : (
+          <p>Loading product details...</p>
+        )}
+        </div>
       </div>
       <Footer />
     </div>
--- front-TypeScript/src/page/home/Home.tsx
+++ front-TypeScript/src/page/home/Home.tsx
@@ -37,18 +37,26 @@ const Home: React.FC = () => {
   return (
     <div className="container">
       <Header />
-      <div id="context" style={{display:'grid', gridTemplateColumns: 'repeat(4, 220px)', gap: '40px', width:'1000px', height:'100vh', margin:'40px 0'}}>
-        {products.map((e) => (
-          <div key={e.id} style={{width:'220px', height:'280px', background:'white', display:'flex', flexDirection:'column', alignItems:'center', borderRadius:'10px', overflow:'hidden'}}>
-            <Link to={`/product/${e.id}`} style={{ textDecoration: 'none' }}> 
-                <img className="card-img-top" src={e.images[0]} alt="Card image cap" style={{width: "220px", height:'180px', objectFit:'fill'}}/>
-                <div className="card-body">
-                  <h1 style={{fontSize:'14px', padding:' 0 6px'}}>{e.product_name}</h1>
-                  <h4>${e.price}</h4>
-                </div> 
-            </Link>
-          </div>
-        ))}
+      <div style={{display:'flex', justifyContent:'center',alignItems:'start', width:'1000px', height:'100vh', margin:'40px 0'}}>
+        <div style={{display:'grid', gridTemplateColumns:'200px 200px 200px 200px', columnGap:'60px', rowGap:'40px' , width:'1000px'}}>
+          {products.map((e) => (
+            <div className="product-card" key={e.id} style={{width:'220px', height:'300px', background:'white', display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'space-between', borderRadius:'8px', overflow:'hidden', border:''}}>
+              <div style={{width: "220px", height:'200px'}}>
+                <img src={e.images[0]} alt="Card image cap" style={{width: "220px", height:'200px', objectFit:'cover'}}/>
+              </div>
+              <div style={{width:'220px', height:'90px', display:'flex', flexDirection:'column'}}>
+                <div style={{width:'220px', height:'45px', padding:'0 10px'}}>
+                  <Link className="product-name" to={`/product/${e.id}`} style={{ textDecoration: 'none' }}>
+                    <h1 style={{fontSize:'14px', color:'black',}}>{e.product_name}</h1>
+                  </Link>
+                </div>   
+                <div style={{width:'220px', height:'45px', display:'flex', justifyContent:'end'}}>
+                  <h4 style={{paddingRight:'10px', color:'red'}}>${e.price}</h4>
+                </div>
+              </div> 
+            </div>
+          ))}
+        </div>
       </div>
       <Footer />
     </div>
